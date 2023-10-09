ffe288dd9c334caefa9d82be130f4f84579c5cae
George Hodulik <ghodulik95@gmail.com>
2023-10-09 16:04:40 -0400

    Change port

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static('public'));  // Serve static files from the 'public' directory
 
 // Serve the index.html file
@@ -26,6 +28,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
     console.log('Server listening on port 3000');
 });
